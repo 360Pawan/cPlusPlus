32aa5f7339b9f5bfcc90aaa7637bc1e2f213b11d
PawanJS <contactpawanjs@gmail.com>
2021-03-17 10:05:59 +0530

    Added patterns

--- numberPattern.cpp
+++ numberPattern.cpp
@@ -9,8 +9,8 @@ int main(){
        for(int j=1;j<=n-i;j++){
            cout<<" ";
        }
-       for(int j=1;j<=n;j++){
-           cout<<"*";
+       for(int j=1;j<=i;j++){
+           cout<<j<<" ";
        }
        cout<<endl;
    }
